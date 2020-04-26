678664bb37cc79401d93b67ae171977133419a55
Brandon K. Waltz <bkwaltz@colby.edu>
2020-04-26 14:03:08 -0400

    uses final masonry_size meta field

--- src/Masonry.js
+++ src/Masonry.js
@@ -47,7 +47,7 @@ export default class Requester extends React.Component {
     renderChildren = () =>
         this.state.data.slice(0, this.state.limit).map(element => {
             let size = '4';
-            if (element.size === 'large') {
+            if (element.meta.masonry_size === 'large') {
                 size = '8';
             }
             if (this.props.type === 'card') {
